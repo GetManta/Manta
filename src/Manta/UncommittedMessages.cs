17160face418a0ed742806d8ac9fa152f64fce9c
Dariusz Lenartowicz <dariusz.lenartowicz@softio.pl>
2018-05-18 13:53:58 +0200

    Commit metadata byte[] -> ArraySegment<byte>

--- src/Manta/UncommittedMessages.cs
+++ src/Manta/UncommittedMessages.cs
@@ -4,7 +4,7 @@ namespace Manta
 {
     public class UncommittedMessages
     {
-        public UncommittedMessages(Guid correlationId, MessageRecord[] messages, byte[] commitMetadata = null)
+        public UncommittedMessages(Guid correlationId, MessageRecord[] messages, ArraySegment<byte>? commitMetadata = null)
         {
             CorrelationId = correlationId;
             Messages = messages;
@@ -12,7 +12,7 @@ namespace Manta
         }
 
         public Guid CorrelationId { get; }
-        public byte[] CommitMetadata { get; }
+        public ArraySegment<byte>? CommitMetadata { get; }
         public MessageRecord[] Messages { get; }
     }
 }
